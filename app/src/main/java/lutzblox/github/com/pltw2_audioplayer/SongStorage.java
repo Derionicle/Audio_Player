36a710e393630290a6ad74805424e3ca58efc2ab
Derionicle <Derionicle@users.noreply.github.com>
2016-01-15 10:07:10 -0500

    Create SongStorage.java

---
+++ app/src/main/java/lutzblox/github/com/pltw2_audioplayer/SongStorage.java
@@ -0,0 +1,10 @@
+package lutzblox.github.com.pltw2_audioplayer;
+
+import android.content.Context;
+import android.media.MediaPlayer;
+
+public class SongStorage {
+//store list of song location?
+//maintain array of songs?
+//write to permanent storage? - Json?
+}
